ffc027693b4062b59ab14491b819d9a24f3de7c7
Sorion <maxime.pelte@gmail.com>
2015-12-23 14:29:45 +0100

    Adding Snake deplacement

--- Game.cs
+++ Game.cs
@@ -9,8 +9,7 @@ namespace Snake_CSharp
     
     class Game
     {
-        List<string> snake;
-        protected int cursorXpos,
+        private int cursorXpos,
           cursorYpos,
           endPos,
           zoneX,
@@ -19,14 +18,11 @@ namespace Snake_CSharp
 
         public Game()
         {
-            snake = new List<string> { "*", "*", "*" };
             cursorXpos = 3;
             cursorYpos = 3;
             zoneX = Console.WindowWidth;
             zoneY = Console.WindowHeight;
             endPos = 0;
-
-
 
         }
 
@@ -34,10 +30,8 @@ namespace Snake_CSharp
         {
             Console.Clear();
             Console.SetCursorPosition(1, 1);
-
             Console.WriteLine("SCORE : " + score);
             setGameZone();
-
         }
 
         private void setGameZone()
@@ -70,139 +64,133 @@ namespace Snake_CSharp
             Engine engine = new Engine(500, zoneX, zoneY);
             Console.CursorVisible = false;
             displayGame(0);
-           // engine.DisplaySnake();
             System.Threading.Thread.Sleep(500);
             updateGame(engine);
             Console.Read();
         }
 
-        public void updateGame(Engine engine)
+        private void updateGame(Engine engine)
         {
-            int speed = 500;
-            while (true)
-            {       
-                System.Threading.Thread.Sleep(speed);
+            //int speed = 500;
+            bool loop = true;
+            ThreadKey keyT = new ThreadKey(4);
+            engine.direction = keyT.direction;
+            keyT.Start();
+            while (loop)
+            {
+                System.Threading.Thread.Sleep(500);
+                engine.direction = keyT.direction;
                 engine.ClearSnake();
-                //displayGame(0);
-                engine.UpdatePos(1);
+                engine.UpdatePos();
                 engine.DisplaySnake();
+
             }
+        }
+    }
+    public class Engine
+    {
+        private static byte UP = 1, DOWN = 2, LEFT = 3, RIGHT = 4;
+        public byte direction;
+        private int zoneX, zoneY;
+        private Snake<IndexedChar> snake;
+
+
+        public Engine(ushort speed, int x, int y)
+        {
+            zoneX = x;
+            zoneY = y;
+            initSnake(3);
         }
 
 
-        public class Engine
+        protected void initSnake(byte nbElement)
         {
-            private static byte UP = 1,
-                             DOWN = 2,
-                             LEFT = 3,
-                             RIGHT = 4;
+            snake = new Snake<IndexedChar>();
 
-            protected int zoneX, zoneY;
-            private Snake<IndexedChar> snake;
-            private byte direction;
-            private ushort speed;
-
-            public Engine(ushort speed, int x, int y)
+            for (byte i = 0; i < nbElement; i++)
             {
-                direction = RIGHT;
-                this.speed = speed;
-                zoneX = x;
-                zoneY = y;
-                initSnake(3);
+                IndexedChar snakeElem = new IndexedChar();
+                snakeElem.Char = "*";
+                snakeElem.x = zoneX / 2 + (int)i;
+                snakeElem.y = zoneY / 2;
+                snake.Add(snakeElem);
             }
+        }
 
+        public void DisplaySnake()
+        {
+            ElemS<IndexedChar> last = snake.First;
 
-            protected void initSnake(byte nbElement)
+            while (last != snake.Last)
             {
-                snake = new Snake<IndexedChar>();
-
-                for (byte i = 0; i < nbElement; i++)
-                {
-                    IndexedChar snakeElem = new IndexedChar();
-                    snakeElem.Char = "*";
-                    snakeElem.Direction = direction;
-                    
-                    snakeElem.x = zoneX / 2 + (int)i;
-                    snakeElem.y = zoneY / 2;
-                    snake.Add(snakeElem);
-                }
+                Console.SetCursorPosition(last.Value.x, last.Value.y);
+                Console.Write(last.Value.Char);
+                last = last.Next;
             }
+            Console.SetCursorPosition(snake.Last.Value.x, snake.Last.Value.y);
+            Console.Write(snake.Last.Value.Char);
+        }
 
-            public void DisplaySnake()
+        public static void ClearCurrentConsoleLine()
+        {
+            int currentLineCursor = Console.CursorTop;
+            Console.SetCursorPosition(4, Console.CursorTop);
+            Console.Write(new string(' ', Console.WindowWidth - 8));
+            Console.SetCursorPosition(0, currentLineCursor);
+        }
+
+        public void ClearSnake()
+        {
+            ElemS<IndexedChar> last = snake.First;
+
+            while (last != snake.Last)
             {
-                    ElemS<IndexedChar> last = snake.First;
-                
-                    while(last != snake.Last)
-                    {
-                        Console.SetCursorPosition(last.Value.x, last.Value.y);
-                        Console.Write(last.Value.Char);
-                        last = last.Next;
-                    }
-                Console.SetCursorPosition(snake.Last.Value.x, snake.Last.Value.y);
-                Console.Write(snake.Last.Value.Char);
-            }
-
-            public static void ClearCurrentConsoleLine()
-            {
-                int currentLineCursor = Console.CursorTop;
-                Console.SetCursorPosition(4, Console.CursorTop);
-                Console.Write(new string(' ', Console.WindowWidth-8));
-                Console.SetCursorPosition(0, currentLineCursor);
-            }
-
-            public void ClearSnake()
-            {
-                ElemS<IndexedChar> last = snake.First;
-
-                while (last != snake.Last)
-                {
-                    Console.SetCursorPosition(last.Value.x, last.Value.y);
-                    ClearCurrentConsoleLine();
-                    last = last.Next;
-                }
-                Console.SetCursorPosition(snake.Last.Value.x, snake.Last.Value.y);
+                Console.SetCursorPosition(last.Value.x, last.Value.y);
                 ClearCurrentConsoleLine();
+                last = last.Next;
+            }
+            Console.SetCursorPosition(snake.Last.Value.x, snake.Last.Value.y);
+            ClearCurrentConsoleLine();
+        }
+
+
+        public void UpdatePos()
+        {
+            IndexedChar temp = new IndexedChar();
+
+            ElemS<IndexedChar> last = snake.Last;
+
+            while (last != snake.First)
+            {
+                temp = last.Value;
+                temp = last.Previous.Value;
+                last.Value = temp;
+                last = last.Previous;
             }
 
-
-            public void UpdatePos(byte direction)
+            if (direction == UP)
             {
-                IndexedChar temp = new IndexedChar();
-
-                ElemS<IndexedChar> last = snake.Last;
-
-                while(last != snake.First)
-                {
-                    temp = last.Value;
-                    temp = last.Previous.Value;
-                    last.Value = temp;
-                    last = last.Previous;
-                }
-
-                if(direction == UP)
-                {
-                    temp = snake.First.Value;
-                    temp.y -= 1;
-                    snake.First.Value = temp;
-                }
-                else if(direction == DOWN)
-                {
-                    temp = snake.First.Value;
-                    temp.y += 1;
-                    snake.First.Value = temp;
-                }
-                else if(direction == LEFT)
-                {
-                    temp = snake.First.Value;
-                    temp.x -= 1;
-                    snake.First.Value = temp;
-                }
-                else if(direction == RIGHT)
-                {
-                    temp = snake.First.Value;
-                    temp.x += 1;
-                    snake.First.Value = temp;
-                }
+                temp = snake.First.Value;
+                temp.y -= 1;
+                snake.First.Value = temp;
+            }
+            else if (direction == DOWN)
+            {
+                temp = snake.First.Value;
+                temp.y += 1;
+                snake.First.Value = temp;
+            }
+            else if (direction == LEFT)
+            {
+                temp = snake.First.Value;
+                temp.x -= 1;
+                snake.First.Value = temp;
+            }
+            else if (direction == RIGHT)
+            {
+                temp = snake.First.Value;
+                temp.x += 1;
+                snake.First.Value = temp;
             }
         }
     }
